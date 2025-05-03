61c741d2d393cd1d4c48af513f7d5141365d1efd
Carolina <carol.leamari@gmail.com>
2025-05-03 11:44:01 -0300

    corrigindo link da API

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-fetch('https://date-time-api.onrender.com/datetime')
+fetch('https://apidatahora-3.onrender.com/datetime')
   .then(response => response.json())
   .then(data => {
     document.getElementById('datetime').innerText = `Data: ${data.date} | Hora: ${data.time}`;
   })
   .catch(error => {
     document.getElementById('datetime').innerText = 'Erro ao carregar data/hora';
+    console.error(error);
   });
